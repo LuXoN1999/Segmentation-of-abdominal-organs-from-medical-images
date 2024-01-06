baeed972daea7dc8e5888596c850db29ca78b613
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-07 00:47:52 +0100

    Add methods for encoder/decoder block generation

--- unet/__init__.py
+++ unet/__init__.py
@@ -17,3 +17,15 @@ class UnetBuilder:
         curr_layer = Activation(activation="relu")(curr_layer)
         return curr_layer
 
+    @classmethod
+    def encoder_block(cls, inputs, num_filters):
+        curr_layer = UnetBuilder.conv_block(inputs=inputs, num_filters=num_filters)
+        pooling_layer = MaxPool2D(pool_size=(2, 2))(curr_layer)
+        return curr_layer, pooling_layer
+
+    @classmethod
+    def decoder_block(cls, inputs, skip_conn_layer, num_filters):
+        curr_layer = Conv2DTranspose(filters=num_filters, kernel_size=2, strides=2, padding="same")(inputs)
+        curr_layer = Concatenate()([curr_layer, skip_conn_layer])
+        curr_layer = UnetBuilder.conv_block(inputs=curr_layer, num_filters=num_filters)
+        return curr_layer
