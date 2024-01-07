7e5201a302614cc0c9c801f5a9b1c67e6a7653de
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-07 01:01:12 +0100

    Add method for building U-Net model

--- unet/__init__.py
+++ unet/__init__.py
@@ -3,6 +3,30 @@ from tensorflow.keras.models import Model
 
 
 class Unet:
+    @classmethod
+    def build_unet(cls, input_shape: tuple[int, int, int], num_classes: int, summarize: bool = False) -> Model:
+        inputs = Input(shape=input_shape)
+        encoder_block, conv_block, decoder_block = UnetBuilder.encoder_block, UnetBuilder.conv_block, UnetBuilder.decoder_block
+        # encoder block
+        s1, p1 = encoder_block(inputs=inputs, num_filters=64)
+        s2, p2 = encoder_block(inputs=p1, num_filters=128)
+        s3, p3 = encoder_block(inputs=p2, num_filters=256)
+        s4, p4 = encoder_block(inputs=p3, num_filters=512)
+        # bottleneck block
+        b1 = conv_block(inputs=p4, num_filters=1024)
+        # decoder block
+        d1 = decoder_block(inputs=b1, skip_conn_layer=s4, num_filters=512)
+        d2 = decoder_block(inputs=d1, skip_conn_layer=s3, num_filters=256)
+        d3 = decoder_block(inputs=d2, skip_conn_layer=s2, num_filters=128)
+        d4 = decoder_block(inputs=d3, skip_conn_layer=s1, num_filters=64)
+
+        outputs = Conv2D(filters=num_classes, kernel_size=1, padding="same", activation="softmax")(d4)
+
+        model = Model(inputs, outputs)
+        if summarize:
+            model.summary()
+        return model
+
 
 class UnetBuilder:
     @classmethod
