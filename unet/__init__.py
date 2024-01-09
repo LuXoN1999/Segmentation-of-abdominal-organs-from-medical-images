afc4625fec3f19e873dd7d848e1f53073d8856cf
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-10 00:18:48 +0100

    Add method for creating convolutional block

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,2 +1,13 @@
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation
+
+
 class UnetBuilder:
-    pass
+    @classmethod
+    def conv_block(cls, inputs, num_filters):
+        curr_layer = Conv2D(filters=num_filters, kernel_size=3, padding="same")(inputs)
+        curr_layer = BatchNormalization()(curr_layer)
+        curr_layer = Activation(activation="relu")(curr_layer)
+        curr_layer = Conv2D(filters=num_filters, kernel_size=3, padding="same")(curr_layer)
+        curr_layer = BatchNormalization()(curr_layer)
+        curr_layer = Activation(activation="relu")(curr_layer)
+        return curr_layer
