13d58f8a2bb9fee9b5acb41c646cbb082f1acf37
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-10 00:49:22 +0100

    Create function for reading single DICOM mask

--- tools/data_loading.py
+++ tools/data_loading.py
@@ -1,6 +1,9 @@
 from glob import glob
 import os
 from pydicom import dcmread
+import numpy as np
+
+
 def load_data(path):
     train_x, valid_x = sorted(glob(os.path.join(path, "train", "img", "*"))), sorted(glob(os.path.join(path, "valid", "img", "*")))
     train_y, valid_y = sorted(glob(os.path.join(path, "train", "msk", "*"))), sorted(glob(os.path.join(path, "valid", "msk", "*")))
@@ -13,3 +16,10 @@ def read_image(path):
     image = image / 255.0
     return image
 
+
+def read_mask(path):
+    path = path.decode()
+    mask = dcmread(fp=path).pixel_array
+    mask = mask / 255.0
+    mask = np.expand_dims(a=mask, axis=-1)
+    return mask
