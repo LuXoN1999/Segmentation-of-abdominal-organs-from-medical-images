4ddf9b8a04c4c0d4b4d50db2fa748cd434652212
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-10 00:24:24 +0100

    Add method for creating decoder block

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate
 
 
 class UnetBuilder:
@@ -17,3 +17,10 @@ class UnetBuilder:
         curr_layer = cls.conv_block(inputs=inputs, num_filters=num_filters)
         pooling_layer = MaxPool2D(pool_size=(2, 2))(curr_layer)
         return curr_layer, pooling_layer
+
+    @classmethod
+    def decoder_block(cls, inputs, skip_conn_layer, num_filters):
+        curr_layer = Conv2DTranspose(filters=num_filters, kernel_size=(2, 2), strides=2, padding="same")(inputs)
+        curr_layer = Concatenate()([curr_layer, skip_conn_layer])
+        curr_layer = cls.conv_block(inputs=curr_layer, num_filters=num_filters)
+        return curr_layer
