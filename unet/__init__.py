278ecc77e959886cf710de566bd3e50fbd6d157f
Luka Lovretić <luka.lovretic1@dice.hr>
2024-01-10 00:21:13 +0100

    Add method for creating encoder block

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D
 
 
 class UnetBuilder:
@@ -11,3 +11,9 @@ class UnetBuilder:
         curr_layer = BatchNormalization()(curr_layer)
         curr_layer = Activation(activation="relu")(curr_layer)
         return curr_layer
+
+    @classmethod
+    def encoder_block(cls, inputs, num_filters):
+        curr_layer = cls.conv_block(inputs=inputs, num_filters=num_filters)
+        pooling_layer = MaxPool2D(pool_size=(2, 2))(curr_layer)
+        return curr_layer, pooling_layer
