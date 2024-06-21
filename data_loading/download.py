11ec9e40b8385dd65fd2ae3f2cefcd89cdac94eb
LuXoN1999 <llovretic99@gmail.com>
2024-06-21 23:06:10 +0200

    Use project root as extraction path (#7)

--- data_loading/download.py
+++ data_loading/download.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from pathlib import Path
 from zipfile import ZipFile
 
 import wget
@@ -13,13 +14,22 @@ def __get_progress_bar(current, total, width=80):
     sys.stdout.flush()
 
 
+def __get_project_path() -> str:
+    current_file_path = Path(__file__).resolve()
+    project_marker = 'README.md'  # using README.md as marker for base level of project
+    for parent in current_file_path.parents:
+        if (parent / project_marker).exists():
+            return str(parent.resolve())
+    raise ValueError("Project path not found.")
+
+
 def __unzip_dataset():
     print("\nExtracting dataset...")
     zip_file_path = "./CHAOS_dataset.zip"
     if not os.path.exists(zip_file_path):
         raise FileNotFoundError("Dataset zip file is not found in current directory.")
     with ZipFile(file=zip_file_path, mode="r") as zip_reference:
-        zip_reference.extractall()
+        zip_reference.extractall(path=__get_project_path())
         print("Dataset extracted!")
     os.remove(path=zip_file_path)
 
