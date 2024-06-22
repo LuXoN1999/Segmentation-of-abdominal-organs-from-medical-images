c4d46e03129b96c81b5f5a48d90fd253a09c96b9
LuXoN1999 <llovretic99@gmail.com>
2024-06-23 00:10:48 +0200

    Move get_project_path function to __init__.py (#4)

--- data_loading/__init__.py
+++ data_loading/__init__.py
@@ -1,4 +1,15 @@
+from pathlib import Path
+
 from torch.utils.data import Dataset
+
+
+def _get_project_path() -> str:
+    current_file_path = Path(__file__).resolve()
+    project_marker = 'README.md'  # using README.md as marker for base level of project
+    for parent in current_file_path.parents:
+        if (parent / project_marker).exists():
+            return str(parent.resolve())
+    raise ValueError("Project path not found.")
 
 
 def _validate_params(dataset_type: str, validation_split: float):
