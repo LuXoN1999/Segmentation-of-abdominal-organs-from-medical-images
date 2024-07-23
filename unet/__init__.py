625081bff55149da5c141e12c3804de180b2cee0
LuXoN1999 <llovretic99@gmail.com>
2024-07-23 21:51:27 +0200

    Define convolutional block (#5)

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,4 +1,13 @@
-from torch.nn import Module
+from torch.nn import Module, Sequential, Conv2d, ReLU
+
+
+def conv_block(in_channels: int, out_channels: int):
+    return Sequential(
+        Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1),
+        ReLU(inplace=True),
+        Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1),
+        ReLU(inplace=True)
+    )
 
 
 class UNet(Module):
