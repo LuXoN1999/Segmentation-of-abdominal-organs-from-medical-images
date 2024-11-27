fedf2ae244ed05439d423ad88ac6209d1beb91c1
LuXoN1999 <llovretic99@gmail.com>
2024-11-27 23:21:50 +0100

    Define function for overall loss (#13)

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,4 +1,14 @@
-from torch import Tensor
+from torch import Tensor, sigmoid
+from torch.nn import functional
+
+
+def calculate_loss(prediction: Tensor, ground_truth: Tensor, bce_weight: float = 0.5) -> Tensor:
+    bce = functional.binary_cross_entropy_with_logits(input=prediction, target=ground_truth)
+    prediction = sigmoid(input=prediction)
+    dice = dice_loss(prediction=prediction, ground_truth=ground_truth)
+    loss = bce * bce_weight + dice * (1 - bce_weight)
+    return loss
+
 
 def dice_loss(prediction: Tensor, ground_truth: Tensor, smooth: float = 1.) -> Tensor:
     intersection = (prediction * ground_truth).sum(dim=2).sum(dim=2)
