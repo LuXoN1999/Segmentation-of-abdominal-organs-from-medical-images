85f7e1bdc41279446799f87991a961a95be954f6
LuXoN1999 <llovretic99@gmail.com>
2025-01-19 22:29:00 +0100

    Refactor UNet class constructor (#16)

--- unet/__init__.py
+++ unet/__init__.py
@@ -24,29 +24,34 @@ def up_conv(in_channels: int, out_channels: int) -> Sequential:
 
 
 class UNet(Module):
-
     def __init__(self, n_classes: int):
         super().__init__()
 
-        self.max_pool = MaxPool2d(kernel_size=2)
-        self.up_conv = Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.max_pool = MaxPool2d(kernel_size=2, stride=2)
 
-        # encoder blocks
+        # encoder path
         self.enc_conv1 = conv_block(in_channels=3, out_channels=64)
         self.enc_conv2 = conv_block(in_channels=64, out_channels=128)
         self.enc_conv3 = conv_block(in_channels=128, out_channels=256)
         self.enc_conv4 = conv_block(in_channels=256, out_channels=512)
+        # bottleneck encoder
+        self.enc_conv5 = conv_block(in_channels=512, out_channels=1024)
 
-        # bottleneck block
-        self.b_conv = conv_block(in_channels=512, out_channels=1024)
+        # decoder path
+        self.up_conv5 = up_conv(in_channels=1024, out_channels=512)
+        self.dec_conv5 = conv_block(in_channels=1024, out_channels=512)
 
-        # decoder blocks
-        self.dec_conv4 = conv_block(in_channels=1024 + 512, out_channels=512)
-        self.dec_conv3 = conv_block(in_channels=512 + 256, out_channels=256)
-        self.dec_conv2 = conv_block(in_channels=256 + 128, out_channels=128)
-        self.dec_conv1 = conv_block(in_channels=128 + 64, out_channels=64)
+        self.up_conv4 = up_conv(in_channels=512, out_channels=256)
+        self.dec_conv4 = conv_block(in_channels=512, out_channels=256)
 
-        self.final_layer = Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1)
+        self.up_conv3 = up_conv(in_channels=256, out_channels=128)
+        self.dec_conv3 = conv_block(in_channels=256, out_channels=128)
+
+        self.up_conv2 = up_conv(in_channels=128, out_channels=64)
+        self.dec_conv2 = conv_block(in_channels=128, out_channels=64)
+
+        # final layer
+        self.fin_conv = Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         # encoder path
