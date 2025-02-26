540d1d9e05c4de612388b8ecc57f2a9a87c633f2
LuXoN1999 <llovretic99@gmail.com>
2025-02-26 12:14:31 +0100

    Refactor _get_project_path function (#21)

--- data_loading/__init__.py
+++ data_loading/__init__.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 
-def _get_project_path() -> str:
+def get_project_root() -> Path:
     current_file_path = Path(__file__).resolve()
-    project_marker = 'README.md'  # using README.md as marker for base level of project
+    project_marker = "README.md"  # using README.md as marker for root level of project
     for parent in current_file_path.parents:
         if (parent / project_marker).exists():
-            return str(parent.resolve())
-    raise ValueError("Project path not found.")
+            return parent.resolve()
+    raise ValueError("Project root not found.")
